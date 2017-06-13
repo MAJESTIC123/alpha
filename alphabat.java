d59810e5e3349b1b2d319e4c02e9cc275cbbb091
MAJESTIC123 <karthiece996@gmail.com>
2017-06-13 15:39:18 +0530

    Add files via upload

---
+++ alphabat.java
@@ -0,0 +1,9 @@
+class alpha{
+public static void main(String[ ] args){
+char ch=args[0].charAt(0);	 		
+if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+System.out.println("is a alphabat");
+else
+System.out.println("Not a alphabet");	
+}
+}
